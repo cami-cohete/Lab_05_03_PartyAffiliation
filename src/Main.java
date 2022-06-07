6e5240682594c8c0cd5223e74a0322fa26e38a66
cami-cohete <esquivca@mail.uc.edu>
2022-06-07 10:58:20 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+//    String politicalParty
+//    output “What is your party affiliation [D, R, or I]? ”
+//    input politicalParty
+//    if politicalParty = = D then
+//       output “You get a Democratic Donkey!”
+//    else if politicalParty = = R then
+//       output “You get an Elephant!”
+//    else
+//       output “You get a Man!”
+//    endIf
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
